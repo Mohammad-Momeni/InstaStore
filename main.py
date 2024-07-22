efdcb629b30df55893e394bc43fcb1887af9843f
Mohammad-Momeni <momeni00022@gmail.com>
2024-07-22 11:46:03 +0330

    added addProfile function

--- main.py
+++ main.py
@@ -79,6 +79,87 @@ def listProfiles():
               + "\nFollowers: " + str(profile[4]) + "\nFollowings: " + str(profile[5]))
         print("---------------------------------")
 
+def addProfile(username):
+    try:
+        for profile in profiles:
+            if profile[0] == username:
+                print("This account is already added!")
+                return
+        
+        response = requests.get(f'https://anonyig.com/api/ig/userInfoByUsername/{username}')
+        if (response.status_code != 200):
+            print("There was an error!")
+            return
+        
+        data = json.loads(response.text)
+        data = data['result']['user']
+        
+        if not os.path.exists(path + f"\\{username}"):
+            os.mkdir(path + f"\\{username}")
+        
+        if not os.path.exists(path + f"\\{username}\\Profiles"):
+            os.mkdir(path + f"\\{username}\\Profiles")
+        
+        pk = int(data["pk"])
+        username = data["username"]
+        full_name = data["full_name"]
+        page_name = data["page_name"]
+        if page_name is None:
+            page_name = "NULL"
+        biography = data["biography"]
+        is_private = data["is_private"]
+        if is_private:
+            is_private = 1
+        else:
+            is_private = 0
+        if "public_email" in data.keys():
+            public_email = data["public_email"]
+        else:
+            public_email = "NULL"
+        media_count = data["media_count"]
+        follower_count = data["follower_count"]
+        following_count = data["following_count"]
+        original_profile_pic_link = data["hd_profile_pic_url_info"]["url"]
+        format = original_profile_pic_link[:original_profile_pic_link.index("?")]
+        format = format[format.rindex("."):]
+        original_profile_pic = f"\\{username}\\Profiles\\Profile{format}"
+        small_profile_pic = f"\\{username}\\Profiles\\Profile_thumbnail{format}"
+
+        isDownloaded = downloadLink(original_profile_pic_link, original_profile_pic)
+        if not isDownloaded:
+            for i in range(5):
+                isDownloaded = downloadLink(original_profile_pic_link, original_profile_pic)
+                print(f"this was the {i}'th time {isDownloaded}")
+                if isDownloaded:
+                    break
+            if not isDownloaded:
+                print("There was an error!")
+                return
+
+        if data["has_anonymous_profile_picture"] == True:
+            shutil.copyfile(original_profile_pic, small_profile_pic)
+        else:
+            small_profile_pic_link = data["hd_profile_pic_versions"][0]["url"]
+            format = original_profile_pic_link[:original_profile_pic_link.index("?")]
+            format = format[format.rindex("."):]
+            small_profile_pic = f"\\{username}\\Profiles\\Profile_thumbnail{format}"
+            isDownloaded = downloadLink(small_profile_pic_link, small_profile_pic)
+            if not isDownloaded:
+                for i in range(5):
+                    isDownloaded = downloadLink(small_profile_pic_link, small_profile_pic)
+                    if isDownloaded:
+                        break
+                if not isDownloaded:
+                    print("There was an error!")
+                    return
+
+        dbCursor.execute(f"""INSERT INTO Profile VALUES({pk}, "{username}", "{full_name}", "{page_name}", "{biography}", {is_private},
+                        "{public_email}", {media_count}, {follower_count}, {following_count}, "{original_profile_pic}", "{small_profile_pic}")""")
+        connection.commit()
+
+    except:
+        print("There was an error!")
+
 connection, dbCursor = initialize()
 
 result = dbCursor.execute("""SELECT username, full_name, biography, media_count,
